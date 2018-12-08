ada11dc23446c084270dcd61185c5be7c8e7e443
CaptnJunks <jams.zeigler@gmail.com>
2018-12-08 12:21:43 -0800

    Commit Override Ranked Method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook
+    class RankedGradeBook : BaseGradeBook
     {
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char grade = 'F';
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade > 80)
+                grade = 'A';
+            else if (averageGrade > 60)
+                grade = 'B';
+            else if (averageGrade > 40)
+                grade = 'C';
+            else if (averageGrade > 20)
+                grade = 'D';
+            return grade;
+        }
     }
 }
